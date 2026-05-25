74d44b9a00839e5abce67a5bde8e5d20390cecdd
esore <88977097+IcantAffordSynapse@users.noreply.github.com>
2026-05-25 20:01:50 +1000

    Update 137069154816703.lua

--- src/games/137069154816703.lua
+++ src/games/137069154816703.lua
@@ -14,7 +14,11 @@ return function(section)
                 for _, br in pairs(workspace.EntitiesFolder:GetChildren()) do
                     plr.Character:MoveTo(Vector3.new(-2494, 4, -726))
                     task.wait(0.5)
-                    if not br:GetAttribute("SpawnZone") == 22 or not br.PrimaryPart then
+                    if not br:GetAttribute("SpawnZone") == 22 then
+                        continue
+                    end
+
+                    if not br.PrimaryPart then
                         continue
                     end
 
